bb0bb2f1cb73291730d37970d6ca8f1cf8275da6
David Cramer <dcramer@gmail.com>
2017-07-05 18:03:48 -0700

    various constraint fixes

--- tests/sentry/tasks/test_deletion.py
+++ tests/sentry/tasks/test_deletion.py
@@ -72,9 +72,11 @@ class DeleteOrganizationTest(TestCase):
 
         assert not Organization.objects.filter(id=org.id).exists()
         assert not Environment.objects.filter(id=env.id).exists()
-        assert not ReleaseEnvironment.objects.filter(id=release_env.id).exists()
+        assert not ReleaseEnvironment.objects.filter(
+            id=release_env.id).exists()
         assert not Repository.objects.filter(id=repo.id).exists()
-        assert not ReleaseCommit.objects.filter(organization_id=org.id).exists()
+        assert not ReleaseCommit.objects.filter(
+            organization_id=org.id).exists()
         assert not Release.objects.filter(organization_id=org.id).exists()
         assert not CommitAuthor.objects.filter(id=commit_author.id).exists()
         assert not Commit.objects.filter(id=commit.id).exists()
@@ -130,7 +132,8 @@ class DeleteProjectTest(TestCase):
             status=ProjectStatus.PENDING_DELETION,
         )
         group = self.create_group(project=project)
-        GroupAssignee.objects.create(group=group, project=project, user=self.user)
+        GroupAssignee.objects.create(
+            group=group, project=project, user=self.user)
         GroupMeta.objects.create(group=group, key='foo', value='bar')
         release = Release.objects.create(version='a' * 32,
                                          organization_id=project.organization_id)
@@ -168,7 +171,7 @@ class DeleteProjectTest(TestCase):
         with self.tasks():
             delete_project(object_id=project.id)
 
-        assert not Project.objects.filter(id=project.id).exists()
+        assert not Project.objects.unconstrained_unsafe().filter(id=project.id).exists()
         assert not EnvironmentProject.objects.filter(
             project_id=project.id,
             environment_id=env.id
@@ -187,7 +190,7 @@ class DeleteProjectTest(TestCase):
             with self.tasks():
                 delete_project(object_id=project.id)
 
-        assert Project.objects.filter(id=project.id).exists()
+        assert Project.objects.unconstrained_unsafe().filter(id=project.id).exists()
 
 
 class DeleteTagKeyTest(TestCase):
@@ -198,7 +201,8 @@ class DeleteTagKeyTest(TestCase):
         tk = TagKey.objects.create(key='foo', project=project)
         TagValue.objects.create(key='foo', value='bar', project=project)
         GroupTagKey.objects.create(key='foo', group=group, project=project)
-        GroupTagValue.objects.create(key='foo', value='bar', group_id=group.id, project_id=project.id)
+        GroupTagValue.objects.create(
+            key='foo', value='bar', group_id=group.id, project_id=project.id)
         EventTag.objects.create(
             key_id=tk.id, group_id=group.id, value_id=1, project_id=project.id,
             event_id=1,
@@ -207,8 +211,10 @@ class DeleteTagKeyTest(TestCase):
         project2 = self.create_project(team=team, name='test2')
         group2 = self.create_group(project=project2)
         tk2 = TagKey.objects.create(key='foo', project=project2)
-        gtk2 = GroupTagKey.objects.create(key='foo', group=group2, project=project2)
-        gtv2 = GroupTagValue.objects.create(key='foo', value='bar', group_id=group2.id, project_id=project2.id)
+        gtk2 = GroupTagKey.objects.create(
+            key='foo', group=group2, project=project2)
+        gtv2 = GroupTagValue.objects.create(
+            key='foo', value='bar', group_id=group2.id, project_id=project2.id)
         EventTag.objects.create(
             key_id=tk2.id, group_id=group2.id, value_id=1, project_id=project.id,
             event_id=1,
@@ -217,9 +223,12 @@ class DeleteTagKeyTest(TestCase):
         with self.tasks():
             delete_tag_key(object_id=tk.id)
 
-            assert not GroupTagValue.objects.filter(key=tk.key, project_id=project.id).exists()
-            assert not GroupTagKey.objects.filter(key=tk.key, project=project).exists()
-            assert not TagValue.objects.filter(key=tk.key, project=project).exists()
+            assert not GroupTagValue.objects.filter(
+                key=tk.key, project_id=project.id).exists()
+            assert not GroupTagKey.objects.filter(
+                key=tk.key, project=project).exists()
+            assert not TagValue.objects.filter(
+                key=tk.key, project=project).exists()
             assert not EventTag.objects.filter(key_id=tk.id).exists()
             assert not TagKey.objects.filter(id=tk.id).exists()
 
@@ -365,7 +374,7 @@ class GenericDeleteTest(TestCase):
             with pytest.raises(DeleteAborted):
                 generic_delete('sentry', 'project', object_id=project.id)
 
-        project = Project.objects.get(id=project.id)
+        project = Project.objects.unconstrained_unsafe().get(id=project.id)
         assert project.status == ObjectStatus.VISIBLE
 
     def test_deletes(self):
@@ -376,7 +385,7 @@ class GenericDeleteTest(TestCase):
         with self.tasks():
             generic_delete('sentry', 'project', object_id=project.id)
 
-        assert not Project.objects.filter(id=project.id).exists()
+        assert not Project.objects.unconstrained_unsafe().filter(id=project.id).exists()
 
 
 class DeleteRepoTest(TestCase):
